e616a24d70b5bd90150db8f47a8a7da58c9c7b47
aljokar1x1-debug <aljokar1x1@gmail.com>
2026-06-04 11:20:13 +0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,5 +1,31 @@
-export { default } from "../dist/server/index.js";
+import { createServer } from "node:http";
+
+let handler;
+
+async function getHandler() {
+  if (!handler) {
+    const mod = await import("../dist/server/index.js");
+    handler = mod.default;
+  }
+  return handler;
+}
+
+export default async function (req, res) {
+  const h = await getHandler();
+  const webReq = new Request(
+    `http://${req.headers.host}${req.url}`,
+    {
+      method: req.method,
+      headers: req.headers,
+      body: ["GET", "HEAD"].includes(req.method) ? undefined : req,
+    }
+  );
+  const webRes = await h.fetch(webReq);
+  res.statusCode = webRes.status;
+  webRes.headers.forEach((v, k) => res.setHeader(k, v));
+  res.end(Buffer.from(await webRes.arrayBuffer()));
+}
 
 export const config = {
-  runtime: "edge",
+  runtime: "nodejs",
 };
